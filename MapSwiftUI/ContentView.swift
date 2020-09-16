bd546dfb472b2b70086ffc3ea9216fd839209b86
Kingsley Eoka <kingsleyenoka@gmail.com>
2020-09-16 00:11:27 -0500

    Initial Commit

---
+++ MapSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapSwiftUI
+//
+//  Created by Kingsley Enoka on 9/16/20.
+//  Copyright © 2020 Kingsley Enoka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
